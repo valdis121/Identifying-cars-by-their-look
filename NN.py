9eeb3e112c5bc6d350f653553197f7eeeafb59d9
valdis121 <88030425+valdis121@users.noreply.github.com>
2023-04-20 18:46:04 +0200

    Change NN.py

--- NN.py
+++ NN.py
@@ -1,61 +1,123 @@
-from tensorflow.keras import Input, Model
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Lambda, Dropout
-from tensorflow.keras import backend as K
-import tensorflow as tf
-import  getDataset
-from tensorflow.keras.optimizers import Adam, RMSprop
-from tensorflow.keras.losses import binary_crossentropy
+from multiprocessing import freeze_support
 
-def euclidean_distance(vects):
-    x, y = vects
-    return K.sqrt(K.sum(K.square(x - y), axis=1, keepdims=True))
+import torch
+from torch import nn
+import torch as th
+from torch.utils.data import DataLoader
+from torchgen.context import F
+from getDataset import getSiameseDataset
+from torchvision import transforms
+
+training_csv = 'result.csv'
+training_dir = '../VehicleID_V1.0/image/'
+siamese_dataset = getSiameseDataset(training_csv, training_dir,
+                                        transform=transforms.Compose([transforms.Resize((105,105)),
+                                                                      transforms.ToTensor()
+                                                                      ]))
+
+# create a siamese network
 
 
-def eucl_dist_output_shape(shapes):
-    shape1, shape2 = shapes
-    return (shape1[0], 1)
-def contrastive_loss(y, preds, margin=1):
-	# explicitly cast the true class label data type to the predicted
-	# class label data type (otherwise we run the risk of having two
-	# separate data types, causing TensorFlow to error out)
-	y = tf.cast(y, preds.dtype)
-	# calculate the contrastive loss between the true labels and
-	# the predicted labels
-	squaredPreds = K.square(preds)
-	squaredMargin = K.square(K.maximum(margin - preds, 0))
-	loss = K.mean(y * squaredPreds + (1 - y) * squaredMargin)
-	# return the computed contrastive loss to the calling function
-	return loss
-def create_siamese_network(input_shape):
-    input1 = Input(shape=input_shape)
-    input2 = Input(shape=input_shape)
 
-    model = tf.keras.Sequential()
-    model.add(Conv2D(124, (3, 3), activation='relu', input_shape=input_shape))
-    model.add(MaxPooling2D((2, 2)))
-    model.add(Conv2D(256, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((2, 2)))
-    model.add(Conv2D(124, (3, 3), activation='relu'))
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.1))
-    model.add(Dense(50, activation='relu'))
-    encoded1 = model(input1)
-    encoded2 = model(input2)
+class SiameseNetwork(nn.Module):
+    def __init__(self):
+        super(SiameseNetwork, self).__init__()
+        # Setting up the Sequential of CNN Layers
+        self.cnn1 = nn.Sequential(
+            nn.Conv2d(1, 96, kernel_size=11, stride=1),
+            nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(5, alpha=0.0001, beta=0.75, k=2),
+            nn.MaxPool2d(3, stride=2),
 
-    distance = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([encoded1, encoded2])
+            nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(5, alpha=0.0001, beta=0.75, k=2),
+            nn.MaxPool2d(3, stride=2),
+            nn.Dropout2d(p=0.3),
 
-    siamese_net = Model(inputs=[input1, input2], outputs=distance)
+            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
 
-    return siamese_net
+            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, stride=2),
+            nn.Dropout2d(p=0.3),
+        )
+        # Defining the fully connected layers
+        self.fc1 = nn.Sequential(
+            nn.Linear(30976, 1024),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.5),
 
-trainDg = getDataset.getSiameseDataset('result.csv', '../VehicleID_V1.0/image/', 128, 64)
-valDg = getDataset.getSiameseDataset('val.csv', '../VehicleID_V1.0/image/', 128, 64)
+            nn.Linear(1024, 128),
+            nn.ReLU(inplace=True),
 
-model = create_siamese_network((64, 64) + (3,))
-rms = RMSprop()
-# Компиляция модели
-model.compile(optimizer="adam", loss=contrastive_loss, metrics=['accuracy'])
+            nn.Linear(128, 2))
 
-# Тренировка модели
-model.fit(trainDg, epochs=10, validation_data=valDg)
+    def forward_once(self, x):
+        # Forward pass
+        output = self.cnn1(x)
+        output = output.view(output.size()[0], -1)
+        output = self.fc1(output)
+        return output
+
+    def forward(self, input1, input2):
+        # forward pass of input 1
+        output1 = self.forward_once(input1)
+        # forward pass of input 2
+        output2 = self.forward_once(input2)
+        return output1, output2
+
+class ContrastiveLoss(torch.nn.Module):
+    def __init__(self, margin=2.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, output1, output2, label):
+        # Find the pairwise distance or eucledian distance of two output feature vectors
+        euclidean_distance = torch.nn.functional.pairwise_distance(output1, output2)
+        # perform contrastive loss calculation with the distance
+        loss_contrastive = torch.mean((1 - label) * torch.pow(euclidean_distance, 2) +
+                                      (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0),
+                                                          2))
+
+        return loss_contrastive
+
+train_dataloader = DataLoader(siamese_dataset, num_workers=1, batch_size=1, shuffle=True)
+# Declare Siamese Network
+net = SiameseNetwork().cuda()
+#net = SiameseNetwork()
+# Decalre Loss Function
+criterion = ContrastiveLoss()
+# Declare Optimizer
+optimizer = th.optim.Adam(net.parameters(), lr=1e-3, weight_decay=0.0005)
+
+
+def train():
+    loss = []
+    counter = []
+    iteration_number = 0
+    for epoch in range(1, 100):
+        for i, data in enumerate(train_dataloader, 0):
+            img0, img1, label = data
+            img0, img1, label = img0.cuda(), img1.cuda(), label.cuda()
+            #img0, img1, label = img0, img1, label
+            optimizer.zero_grad()
+            output1, output2 = net(img0, img1)
+            loss_contrastive = criterion(output1, output2, label)
+            loss_contrastive.backward()
+            optimizer.step()
+            if i%10 == 0:
+                print("Epoch {}\n Current loss {}\n Image {}/{}\n".format(epoch, loss_contrastive.item(), i, len(train_dataloader)))
+        print("Epoch {}\n Current loss {}\n".format(epoch, loss_contrastive.item()))
+        iteration_number += 10
+        counter.append(iteration_number)
+        loss.append(loss_contrastive.item())
+    return net
+
+    # set the device to cuda
+if __name__ == '__main__':
+    device = torch.device('cuda' if th.cuda.is_available() else 'cpu')
+    model = train()
+    torch.save(model.state_dict(), "model.pt")
+    print("Model Saved Successfully")
--- getDataset.py
+++ getDataset.py
@@ -1,54 +1,37 @@
+import os
+
 import pandas as pd
-import numpy as np
-import tensorflow as tf
 from PIL import Image
-import cv2
+import numpy as np
+import torch as th
+import getDataset
 
-class SiameseDataset(tf.keras.utils.Sequence):
-    def __init__(self, csv_path, img_dir, batch_size, img_size):
-        self.df = pd.read_csv(csv_path, dtype={'car1': object, 'car2':object})
-        self.img_dir = img_dir
-        self.batch_size = batch_size
-        self.img_size = img_size
+#preprocessing and loading the dataset
+class SiameseDataset():
+    def __init__(self,training_csv=None, training_dir=None, transform=None):
+        # used to prepare the labels and images path
+        self.train_df = pd.read_csv(training_csv, dtype={"car1": object, "car2": object})
+        self.train_df.columns =["image1", "image2", "label"]
+        self.train_dir = training_dir    
+        self.transform = transform
 
+    def __getitem__(self, index):
+        # getting the image path
+        image1_path = os.path.join(self.train_dir, self.train_df.iat[index, 0] + ".jpg")
+        image2_path = os.path.join(self.train_dir, self.train_df.iat[index, 1] + ".jpg")
+        # Loading the image
+        img0 = Image.open(image1_path)
+        img1 = Image.open(image2_path)
+        img0 = img0.convert("L")
+        img1 = img1.convert("L")
+        # Apply image transformations
+        if self.transform is not None:
+            img0 = self.transform(img0)
+            img1 = self.transform(img1)
+        return img0, img1, th.from_numpy(np.array([int(self.train_df.iat[index,2])],dtype=np.float32))
     def __len__(self):
-        return int(np.ceil(len(self.df) / float(self.batch_size)))
+        return len(self.train_df)
 
-    def __getitem__(self, idx):
-        batch_df = self.df.iloc[idx * self.batch_size:(idx + 1) * self.batch_size].reset_index(drop=True)
-        batch_df = batch_df.iloc[np.random.permutation(len(batch_df))]  # перемешиваем индексы строк
-        batch_images1 = []
-        batch_images2 = []
-        batch_labels = []
-        for i, row in batch_df.iterrows():
-            img1 = cv2.imread("{}{}.jpg".format(self.img_dir, row['car1']))
-            img1 = cv2.resize(img1, (self.img_size, self.img_size))
-            batch_images1.append(img1)
+def getSiameseDataset(training_csv=None, training_dir=None, transform=None):
+    return SiameseDataset(training_csv, training_dir, transform)
 
-            img2 = cv2.imread("{}{}.jpg".format(self.img_dir, row['car2']))
-            img2 = cv2.resize(img2, (self.img_size, self.img_size))
-            batch_images2.append(img2)
-
-            batch_labels.append(row['distance'])
-        return [np.array(batch_images1), np.array(batch_images2)], np.array(batch_labels)
-
-    def get_batch_pairs(self, batch_size):
-        batch_df = self.df.sample(batch_size)
-        batch_images1 = []
-        batch_images2 = []
-        batch_labels = []
-        for i, row in batch_df.iterrows():
-            img1 = cv2.imread("{}{}.jpg".format(self.img_dir, row['car1']))
-            img1 = cv2.resize(img1, (self.img_size, self.img_size))
-            batch_images1.append(img1)
-
-            img2 = cv2.imread("{}{}.jpg".format(self.img_dir, row['car2']))
-            img2 = cv2.resize(img2, (self.img_size, self.img_size))
-            batch_images2.append(img2)
-
-            batch_labels.append(row['distance'])
-        return [np.array(batch_images1), np.array(batch_images2)], np.array(batch_labels)
-
-
-def getSiameseDataset(csv_path, img_dir, batch_size, img_size):
-    return SiameseDataset(csv_path, img_dir, batch_size, img_size)
